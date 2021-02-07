0731fd6d28eeca1e41875a0f886d3cbb3de34325
0x435446 <0x435446@gmail.com>
2021-02-07 09:00:32 -0500

    Actualizare interfata web cu date despre conexiuni

--- app.py
+++ app.py
@@ -52,9 +52,14 @@ def select_graph():
 	data2 = (cursor2.fetchall())
 	listen_count=data2[0][0]
 
+	cursor2= db.cursor()
+	cursor2.execute("SELECT Count(Type) FROM alerte WHERE Type='Connections'")
+	data2 = (cursor2.fetchall())
+	connect_count=data2[0][0]
+
 
 	db.close()
-	return icmp_count,dns_count,http_count,vs_count,listen_count
+	return icmp_count,dns_count,http_count,vs_count,listen_count,connect_count
 
 
 @app.route('/')
@@ -71,15 +76,16 @@ def index():
 	cursor = db.cursor()
 	cursor.execute("SELECT * FROM alerte")
 	data = list(cursor.fetchall())
-	icmp_count,dns_count,http_count,vs_count,listen_count=select_graph()
+	icmp_count,dns_count,http_count,vs_count,listen_count,connect_count=select_graph()
 	numar=[]
 	numar.append(str(icmp_count))
 	numar.append(str(dns_count))
 	numar.append(str(http_count))
 	numar.append(str(vs_count))
 	numar.append(str(listen_count))
+	numar.append(str(connect_count))
 	numar.append(str(icmp_count+dns_count+http_count+vs_count+listen_count))
-	return render_template('index.html',data=data,len=len(data),icmp=str(100*icmp_count/len(data))+"%",dns=str(100*dns_count/len(data))+"%",http=str(100*http_count/len(data))+"%",vt=str(100*vs_count/len(data))+"%",lc=str(100*listen_count/len(data)),numar=numar)
+	return render_template('index.html',data=data,len=len(data),icmp=str(100*icmp_count/len(data))+"%",dns=str(100*dns_count/len(data))+"%",http=str(100*http_count/len(data))+"%",vt=str(100*vs_count/len(data))+"%",lc=str(100*listen_count/len(data)),cn=str(100*connect_count/len(data)),numar=numar)
 
 
 @app.route('/stop')
@@ -95,15 +101,16 @@ def stop():
 	cursor.execute("SELECT * FROM alerte")
 	data = list(cursor.fetchall())
 	db.close()
-	icmp_count,dns_count,http_count,vs_count,listen_count=select_graph()
+	icmp_count,dns_count,http_count,vs_count,listen_count,connect_count=select_graph()
 	numar=[]
 	numar.append(str(icmp_count))
 	numar.append(str(dns_count))
 	numar.append(str(http_count))
 	numar.append(str(vs_count))
 	numar.append(str(listen_count))
+	numar.append(str(connect_count))
 	numar.append(str(icmp_count+dns_count+http_count+vs_count+listen_count))
-	return render_template('index.html',data=data,len=len(data),icmp=str(100*icmp_count/len(data))+"%",dns=str(100*dns_count/len(data))+"%",http=str(100*http_count/len(data))+"%",vt=str(100*vs_count/len(data))+"%",lc=str(100*listen_count/len(data)),numar=numar)
+	return render_template('index.html',data=data,len=len(data),icmp=str(100*icmp_count/len(data))+"%",dns=str(100*dns_count/len(data))+"%",http=str(100*http_count/len(data))+"%",vt=str(100*vs_count/len(data))+"%",lc=str(100*listen_count/len(data)),cn=str(100*connect_count/len(data)),numar=numar)
 
 
 if __name__ == '__main__':
