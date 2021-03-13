c54b630a23a37e0620f02337ee2575ce5b7c1f51
0x435446 <0x435446@gmail.com>
2021-03-14 00:42:09 +0200

    Adaugare activitate pentru conexiuni
    Am trecut conexiunile de pe activitatea principala pe activitatea proprie

--- Android/app/src/main/java/com/example/proiect/MainActivity.java
+++ Android/app/src/main/java/com/example/proiect/MainActivity.java
@@ -41,39 +41,7 @@ import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
-    private void check(){
-        Pipe x= Pipe.getInstance();
-        ArrayList<String> result= new ArrayList<String>();
-        List<String> str = new ArrayList<String>();
-        List<String> str2 = new ArrayList<String>();
-        str= Arrays.asList(x.getOutput().toString().split("\n"));
-        for (int i = 1; i < str.size(); i++){
-            if ((!str.get(i).contains("0.0.0.0")) && (!str.get(i).contains(":::"))) {
-                String fanci = str.get(i);
-                List<String> listaString= Arrays.asList(fanci.split(" "));
-                int number = 0;
-                for (int k=0;k<listaString.size();k++) {
-                        String string = listaString.get(k);
-                        if (string != null && !string.isEmpty()) {
-                            number += 1;
-                            try {
-                                if (number == 4 || number == 5 || number == 6)
-                                    result.add(string.replace("::ffff:", "").split(":")[0]);
-                            }
-                            catch (Exception ignored){
 
-                            }
-                        }
-                    }
-
-                System.out.println(result);
-                str2.add(String.valueOf(result));
-            }
-        }
-        GridView g = (GridView) findViewById(R.id.gridView);
-        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(),android.R.layout.simple_list_item_1,result);
-        g.setAdapter(adapter);
-    }
 
 
 
@@ -111,15 +79,15 @@ public class MainActivity extends AppCompatActivity {
                 = new Thread(new Traffic());
         object4.start();
 
-        Button Start = findViewById(R.id.startBtn);
-        Start.setOnClickListener(new View.OnClickListener() {
+
+        Button Conn = findViewById(R.id.startBtn);
+        Conn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                check();
+                Intent it = new Intent(getApplicationContext(), Preview_Connections.class);
+                startActivityForResult(it,3);
             }
-
         });
-
 
         Button Metasploit = findViewById(R.id.startBtn2);
         Metasploit.setOnClickListener(new View.OnClickListener() {
